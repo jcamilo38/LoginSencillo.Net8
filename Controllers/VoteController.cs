fcffca9b65c256fc95548d68a6b1fbb990e1a4cb
Juan Camilo Ossa <ossaguzmanjuancamilo@gmail.com>
2024-01-17 16:10:05 -0500

    LOGIN ASPNETCORE M

---
+++ Controllers/VoteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class VoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
